8bc34a1aa06ec36758872388438f048e34e63271
kiwon <kiwon132445@gmail.com>
2020-02-10 14:22:29 -0800

    minor listview try

--- app/src/main/java/ca/bcit/assignment1/DisplayListActivity.java
+++ app/src/main/java/ca/bcit/assignment1/DisplayListActivity.java
@@ -1,6 +1,9 @@
 package ca.bcit.assignment1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class DisplayListActivity extends AppCompatActivity {
@@ -8,5 +11,9 @@ public class DisplayListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+    }
+    public void onClickDetails(View v) {
+        Intent i = new Intent(this, DataDisplayActivity.class);
+        startActivity(i);
     }
 }
