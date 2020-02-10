f331b441bd5e9e1bcb923a10956cc678cdfcaff0
kiwon <kiwon132445@gmail.com>
2020-02-10 13:16:58 -0800

    date and userInput string to DisplayList

--- app/src/main/java/ca/bcit/assignment1/DisplayListActivity.java
+++ app/src/main/java/ca/bcit/assignment1/DisplayListActivity.java
@@ -4,11 +4,6 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class DisplayListActivity extends AppCompatActivity {
-
-    DisplayListActivity(String date, String userInput) {
-
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/ca/bcit/assignment1/MainActivity.java
+++ app/src/main/java/ca/bcit/assignment1/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.EditText;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,14 +22,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onInputClick(View v) {
-        DisplayListActivity disList = new DisplayListActivity(searchText.getText().toString());
         Intent i = new Intent(this, DisplayListActivity.class);
+        i.putExtra("curDate", getCurDate());
+        i.putExtra("userInput", searchText.getText().toString());
         startActivity(i);
     }
 
     public String getCurDate() {
         String date;
         LocalDateTime myDateObj = LocalDateTime.now();
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern(("yyyy-MM-dd"));
+        date = myDateObj.format(dateFormat);
+        System.out.println(date);
         return date;
     }
 }
